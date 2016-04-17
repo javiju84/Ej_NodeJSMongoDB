5ebd5f41a5b3be5667bb04736733c76c3f365cf0
Javier <javiju84@gmail.com>
2016-04-17 20:27:28 +0200

    vistas jade

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 var express = require ("express");
 var app = express();
+
+
+
+
+/*views*/
+app.set("view engine","jade");
+app.get("/", function (req,res){
+	res.render("index");
+});
+
+
+
+
+
+
+
+
+
+
 app.listen(8080);
 console.log("Servidor conectado puerto 8080")
