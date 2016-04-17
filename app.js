eaa32f0acebfd15416e76fcde15a2157a1082bf7
Javier <javiju84@gmail.com>
2016-04-17 20:58:52 +0200

    se crea la ruta index.jade para preparar envio a la basedatos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require ("express");
 var app = express();
+var Database = require("./models/database").Database;//llamamos al Schema,librerias
 
 
 
@@ -10,14 +11,31 @@ app.get("/", function (req,res){
 	res.render("index");
 });
 
+/*imprimir los valores ingresados por consola*/
+app.get("/",function(req,res){
+	Database.find(function(err,doc){
+		console.log(doc);
+		res.render("index");
+	});
+});
 
+/*crea ruta index.jade*/
+app.post("/datavalors",function(req,res){
+	var database = new Database({
+		nombre: req.body.nombre,
+		fecha: req.body.fecha,
+		apertura: req.body.apertura,
+		alza: req.body.alza,
+		bajada: req.body.bajada,
+		cierre: req.body.cierre,
+		volumen: req.body.volumen,
+		ajuste_cierre: req.body.ajuste_cierre
+	});
+	database.save(function(){
+		res.send("Valores guardados")
+	});
+});
 
-
-
-
-
-
-
-
+/*puerto*/
 app.listen(8080);
 console.log("Servidor conectado puerto 8080")
